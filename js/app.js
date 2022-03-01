d36e32090a64e67b470144b489469dd0abe8f96b
sadiqss <sadiq.sau48@gmail.com>
2022-03-01 15:39:10 +0000

    search result shown

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+
+    searchField.value = '';
+    if (searchText == '') {
+        //please write something to display
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data.brand));
+    }
+
+}
